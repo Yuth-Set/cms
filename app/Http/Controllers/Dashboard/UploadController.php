d7cd3db2e02fecf75b4f532d66907760642a08b4
AaronMaru <sai.vichet70@gmail.com>
2015-12-22 15:15:56 +0700

    upload photo

--- app/Http/Controllers/Dashboard/UploadController.php
+++ app/Http/Controllers/Dashboard/UploadController.php
@@ -1,14 +1,25 @@
 <?php
 
 namespace App\Http\Controllers\Dashboard;
-
 use App\Http\Controllers\Controller;
+use App\Upload;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Input;
 
 class UploadController extends Controller {
-    public function file() {
+    public function file(Request $request) {
     }
 
-    public function photo() {
+    public function photo(Request $request) {
+        $file = Input::file('image');
+        $data['name'] = $file->getClientOriginalName();
+        $data['path'] = base_path() . '\public\images';
+        $data['slug'] = Str::slug($file->getClientOriginalName(), '_');
+        $data['type'] = "image";
+        Upload::create($data);
+        $file->move($data['path'], $data['name']); //
+        return back()->with('message', 'file saved!');
     }
 
     public function video() {
